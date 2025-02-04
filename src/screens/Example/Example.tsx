9ccc7f16a59dbecbf8e58ea5e7f57022d1fb2aa1
Amit <amit.dubey@globant.com>
2025-02-05 00:12:37 +0530

    Trial1: adding bug to test the bot

--- src/screens/Example/Example.tsx
+++ src/screens/Example/Example.tsx
@@ -24,7 +24,7 @@ function Example() {
     variant,
   } = useTheme();
 
-  const [currentId, setCurrentId] = useState(-1);
+  const [currentId, setCurrentId] = useState('name');
 
   const fetchOneUserQuery = useFetchOneQuery(currentId);
 
